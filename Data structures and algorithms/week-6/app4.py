49b3be1750497c88b79048cb59143b7dd1011a82
srk0102200 <ramakrishnasiva128@gmail.com>
2022-04-23 17:26:50 +0530

    python week-6 done

--- Data structures and algorithms/week-6/app4.py
+++ Data structures and algorithms/week-6/app4.py
@@ -1,14 +1,17 @@
+from queue import Queue
 class Node:
     def __init__(self, value):
         self.value = value
         self.left = None
         self.right = None
+        self.child = []
 
 
 class Tree:
 
     def __init__(self):
         self.head = None
+        self.child = []
 
     def Inorder(self, p):
         # print(p.left.value)
@@ -48,6 +51,50 @@ class Tree:
                 p = p.right
 
             return False
+    
+    def maxValue(self):
+        current = self.head
+        #loop down to find the rightmost leaf
+        while(current.right):
+            current = current.right
+        return current.value
+    
+    def desc(self, p):
+        # print(p.left.value)
+        if p is None:
+            return
+        else:
+            self.desc(p.right)
+            self.desc(p.left)
+            print(p.value, end=" ")
+
+    def numberOfSiblings(root, x):
+        if (root.head == None):
+            return 0
+    
+        # Creating a queue and
+        # pushing the root
+        q = Queue()
+        q.put(root.head)
+    
+        while (not q.empty()):
+            
+            # Dequeue an item from queue and
+            # check if it is equal to x If YES,
+            # then return number of children
+            p = q.queue[0]
+            q.get()
+    
+            # Enqueue all children of
+            # the dequeued item
+            for i in range(len(p.child)):
+                
+                # If the value of children
+                # is equal to x, then return
+                # the number of siblings
+                if (p.child[i].key == x):
+                    return len(p.child) - 1
+                q.put(p.child[i])
 
 
 root = Tree()
@@ -65,4 +112,8 @@ root.Inorder(p)
 
 print()
 print()
-print("Is element 5 in the tree?" + str(root.Search(5)))
+print("Is element 5 in the tree? " + str(root.Search(5)))
+print("Maxvalue in tree is " + str(root.maxValue()))
+root.desc(p)
+print()
+print(root.numberOfSiblings(4))
