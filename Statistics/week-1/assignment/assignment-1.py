49bfe2f9a6a8ca8485a94f1a49b53633dc08ab9e
srk <ramakrishnasiva128@gmail.com>
2022-05-13 23:51:14 -0700

    assignment-1 done

--- Statistics/week-1/assignment/assignment-1.py
+++ Statistics/week-1/assignment/assignment-1.py
@@ -4,24 +4,20 @@ print("Programming Assignment #1")
 #Function to solve sample mean
 def sampleMean(sm, num, sof, n):
   mean = sof/n
-  if(n>1):
-    pMean = (sof-num)/(n-1)
-  else:
-    pMean = 0
-  sampleMean = pMean + ((mean-pMean)/n)
+  sampleMean = sm + ((mean-sm)/n)
   return sampleMean
 
-def sampleVarience(sv, num, sof, n):
+def sampleVariance(sv, num, sof, n):
   mean = sof/n
   if(n>1):
     pMean = (sof-num)/(n-1)
   else:
     pMean = 0
-  Variance = pow((num-pMean),2)/(n-1)
   if(n>2):
     res = (((n-1)/(n-2))*(sv)) + ((pow((mean-pMean),2))/n)
   else:
     res = 0
+  return res
 
 #Array to store Numbers
 num = int(input("Enter Non-Negative number to calculate Mean and Variance : "))
@@ -29,12 +25,13 @@ num = int(input("Enter Non-Negative number to calculate Mean and Variance : "))
 sumOfNumbers = 0 # Sum of numbers
 sm = 0 # -> Sample Mean
 n=0  # -> Number of values entered by users
+sv = 0 # -> Sample Variance
 
 while(num >=0 ):
   n += 1
   sumOfNumbers += num
   sm = sampleMean(sm, num, sumOfNumbers, n)
-  sv = sampleVarience(sv, num, sumOfNumbers, n)
-  print(sm)
-  print(sv)
+  sv = sampleVariance(sv, num, sumOfNumbers, n)
+  print(sm , "======> Sample Mean")
+  print(sv , "======> Sample Variance")
   num = int(input("Enter Non-Negative number to calculate Mean and Variance : "))
